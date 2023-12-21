7b8f0a0f7540800df68a18a05af0c7616bf2bfe3
35drake <phys152boi@gmail.com>
2023-12-21 14:43:34 -0500

    Primative settings adjustment added.

--- ABE.py
+++ ABE.py
@@ -1,26 +1,46 @@
 import os # To use the type command and easily display a text file's contents
 
+# display the main menu of the program
 def menu_display():	
+	os.system("cls")
+	print ("BULK EMAILING PROGRAM\n---------------")
 	print ("Please enter the number corresponding to the menu option you want to choose:")
 	print ("\t1) Instructions")
-	print ("\t2) Blah")
+	print ("\t2) Settings")
 	print ("\t3) Blah")
 	print ("\t4) Blah")
 	print ("\t5) Blah")
 	print ("\t6) Quit")
 
-print ("BULK EMAILING PROGRAM\n---------------")
+
 
 # Take an integer as a menu choice
 while True:
 	menu_display()
 	menu_choice = int(input())
-
-	if menu_choice == 1:
+	os.system("cls")
+	if menu_choice == 1:	
 		os.system("type instructions.txt")
-
+		unused_variable = input()
 	elif menu_choice == 2:
-		pass
+		submenu_choice = 0
+		while submenu_choice != 3:
+			print("SETTINGS")
+			print("1)\tChange email context (text) ")
+			print("2)\tChange other settings")
+			print("3)\tQuit")
+			submenu_choice = int(input())
+			os.system("cls")
+
+			if submenu_choice == 1:
+				os.system("notepad \"files/Email Text Content.txt\"")
+			elif submenu_choice == 2:
+				os.system("notepad files/variables.txt")
+			elif submenu_choice == 3:
+				pass
+			else:
+				print("Ivalid choice.")
+				unused_variable = input()
 
 	elif menu_choice == 3:
 		pass
@@ -36,4 +56,5 @@ while True:
 
 	else:
 		print("Invalid choice.")
+		unused_variable = input()
 
--- README.txt
+++
@@ -1,2 +0,0 @@
-# Annual-Bulk-Emailer
-If you have a spreadsheet of contacts that you need to send a mass email out to, this program will send out mass emails, and assist you in tracking who has responded.
