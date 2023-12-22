62fe73643b1c2dccd047671e7c9add3829c6d69b
35drake <phys152boi@gmail.com>
2023-12-22 16:32:12 -0500

    Spreadsheet manipulation support started.

--- ABE.py
+++ ABE.py
@@ -4,10 +4,35 @@ import os # For command-line and printing commands
 #	prints them, and updates the spreadsheet when you upload them to your online database. It also reminds you when documents are
 # 	to be uploaded. NOTE: Before it completes any email or printing, it will ask you for confirmation.
 def wizard():
+	backup_spreadsheet()
+	a=input()
 	# get the spreadsheet as a NumPy array 
 	SS = pandas.read_excel("Spreadsheet.xlsx").to_numpy()
 	print(SS)
+	print(type(SS))
+	
+	#Now, do the following:
+		# Print any correctly-named documents that have come in folder 1A, then check them off the spreadsheet.
+		# whatever incorrectly-named documents there are, place them in folder 1B and notify the user.
+		# Create recommended emails with attachments(don't send, but assume that the user did send them)
+		# PROGRAM HERE DRAKE
+	
+	
+	
+	
 	unused_variable = input()
+
+
+# Overwrite the old backup spreadsheet every time a new set of spreadsheet of operations is completed and checked off the sheet.
+# (ie a bunch of prints or a bunch of emails). I don't wanna wait till BOTH sets of operations are done before backup up, because 
+# then if there's a problem with one and not the other, we could be left with only a backup that's too outdated to fall back on.
+def backup_spreadsheet():
+	# Copy the Spreadsheet file as a backup, and overwrite the previous backup if one exists
+	# For some reason I can't figure out how to both copy and rename a file in a single cmd command
+	os.system("copy Spreadsheet.xlsx Backup.xlsx /Y")
+	os.system("move Backup.xlsx files") # moving it into the "files" folder
+
+
 
 
 # Print a document (the default printer must already be set properly)
@@ -63,7 +88,6 @@ except ModuleNotFoundError:
 
 
 
-
 # Take an integer as a menu choice
 while True:
 	menu_display()
@@ -98,10 +122,12 @@ while True:
 
 	elif menu_choice == 4:
 		os.system("cls")
-		print("Running test prints. Make sure that the default printer and print settings are ")
-		print("set properly for both the Word Doc and PDF prints.")
-		print("Word Doc printing",print_document("files\\Test-Prints\\Word-Doc-Test.docx"))  # I'm not sure why slashes don't work here and only double-backslashes do
-		print("PDF printing",print_document("files\\Test-Prints\\PDF-Test.PDF"))
+		print_choice = input("Would you like to run test prints? (Y/n) ")
+		if print_choice == "Y" or print_choice == "y":
+			print("Running test prints. Make sure that the default printer and print settings are ")
+			print("set properly for both the Word Doc and PDF prints.")
+			print("Word Doc printing",print_document("files\\Test-Prints\\Word-Doc-Test.docx"))  # I'm not sure why slashes don't work here and only double-backslashes do
+			print("PDF printing",print_document("files\\Test-Prints\\PDF-Test.PDF"))
 		unused_variable = input("Press Enter to continue.")
 
 	elif menu_choice == 5:
